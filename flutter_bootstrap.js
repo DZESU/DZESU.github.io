ea63243d3d454dcda875271124982989572c1075
Sovann Pory <sovann.pory@wingbank.com.kh>
2024-11-30 15:52:36 +0700

    Test firebase

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4081082965"
+    serviceWorkerVersion: "3756028096"
   }
 });
